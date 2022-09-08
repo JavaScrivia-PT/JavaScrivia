3e2cdf9b3260c6c9573bd0e982781532869828b6
samos17 <amos.serena17@gmail.com>
2022-09-08 15:33:18 -0400

    test all integration routes refactor middleware tests

--- __tests__/middleware.js
+++ __tests__/middleware.js
@@ -1,15 +1,16 @@
-const fs = require('fs');
-const path = require('path');
-const { leaderBoard, getScore, updateProgress } = require('../server/userController.js');
+const {
+  leaderBoard,
+  getScore,
+  updateProgress,
+} = require('../server/userController.js');
 const db = require('../server/userModels');
-
 
 describe('db unit tests', () => {
   beforeAll((done) => {
     const values = ['Test', 'Test', 0];
-    const query = `INSERT INTO user_final (username, password, score) VALUES ($1, $2, $3);`
+    const query = `INSERT INTO user_final (username, password, score) VALUES ($1, $2, $3);`;
     db.query(query, values).then(() => done());
-  })
+  });
   describe('leaderBoard', () => {
     it('returns all usernames and corresponding scores from database as array', async () => {
       const request = {};
@@ -26,7 +27,7 @@ describe('db unit tests', () => {
       expect(Array.isArray(result)).toBe(true);
       expect(Array.isArray(boardPayload)).toBe(true);
       expect(result).toBe(boardPayload);
-      expect(result[result.length - 1]).toEqual({username: 'Test', score: 0})
+      expect(result[result.length - 1]).toEqual({ username: 'Test', score: 0 });
     });
   });
   describe('getScore', () => {
@@ -44,7 +45,7 @@ describe('db unit tests', () => {
       expect(result).not.toBeInstanceOf(Error);
       expect(typeof result).toBe('object');
       expect(data.score).toBe(0);
-      expect(data.progress).toBe(null);
+      expect(data.progress).toBe('0');
     });
   });
   describe('updateProgress', () => {
@@ -65,7 +66,7 @@ describe('db unit tests', () => {
     });
   });
   afterAll((done) => {
-    const query = "DELETE FROM user_final WHERE username = 'Test';"
-    db.query(query).then(()=> done());
-  })
+    const query = "DELETE FROM user_final WHERE username = 'Test';";
+    db.query(query).then(() => done());
+  });
 });
--- __tests__/supertest.js
+++ __tests__/supertest.js
@@ -1,11 +1,10 @@
-const { response } = require('express');
 const request = require('supertest');
 const db = require('../server/userModels');
 
 const server = 'http://localhost:3000';
 
 describe('Route Integration', () => {
-  jest.setTimeout(10000);
+  jest.setTimeout(15000);
   describe('/', () => {
     describe('GET', () => {
       it('responds with status of 200 and text/html content type', () => {
@@ -27,17 +26,17 @@ describe('Route Integration', () => {
   describe('/api', () => {
     describe('POST', () => {
       it('returns true indicating that the user was created', (done) => {
-        const body = {username: 'Test', password: 'Test'};
+        const body = { username: 'Test', password: 'Test' };
         request(server)
           .post('/api')
-          .expect("Content-Type", /application\/json/)
+          .expect('Content-Type', /application\/json/)
           .send(body)
           .expect(200)
           .end((err, res) => {
             if (err) return done(err);
             if (res.body === false) return done('response should be true');
             return done();
-          })
+          });
       });
       describe('GET', () => {
         it('returns true indicating that the user has successfully logged in', (done) => {
@@ -48,60 +47,122 @@ describe('Route Integration', () => {
             .end((err, res) => {
               if (err) return done(err);
               if (!res.body) {
-                return done(`response should be "true". Instead received ${res.body}`)
+                return done(
+                  `response should be "true". Instead received ${res.body}`
+                );
               }
               return done();
-            })
-        })
-      })
+            });
+        });
+      });
       describe('PATCH', () => {
-        it('returns "score updated" to indicate that the score has been updated', (done) => {
-          request(server)
-            .patch('/api')
-            .send({username: 'Test', score: 2})
-            .expect('Content-Type', /application\/json/)
-            .expect(200)
-            .end((err, res) => {
-              if (err) return done(err);
-              if (res.body !== 'score updated') return done(`response should be "score updated". Instead received ${res.body}`);
-              return done();
-            })
-        })
-      })
-    })
-    describe('GET /board', () => {
-      it('returns an array of objects', (done) => {
-        request(server)
-          .get('/board')
-          .expect('Content-Type', /application\/json/)
-          .expect(200)
-          .end((err, res) => {
-            if (err) return done(err);
-            if (!Array.isArray(res.body)) return done(`Expected to receive and array. Instead received ${typeof res.body}`)
-            if (res.body.length < 1) return done(`Expected array of length greater than 0. Instead received length of ${res.body.length}`)
-            if (res.body[res.body.length - 1].username !== 'Test') return done(`Expected username of last element in array to be 'Test'. Instead received ${res.body[res.body.length].username}`)
-            if (res.body[res.body.length - 1].score !== 2) return done(`Expected score of last element in array to be 2. Instead received ${res.body[res.body.length].score}`)
-            return done()
-          })
-      })
-    })
-    describe('GET /user', () => {
-      it('returns score and progress for given user', (done) => {
-        request(server)
-            .get('/user?username=Test')
-            .expect('Content-Type', /application\/json/)
-            .expect(200)
-            .end((err, res) => {
-              if (err) return done(err);
-              if (res.body.score !== 2) return done(`Expected score to be 2. Instead received ${res.body.score}.`)
-              if (res.body.progress !== '0') return done(`Expected progress to be 0. Instead received ${res.body.progress}`)
-              return done();
-            })
-      })
-    })
-  })
+        describe('/api', () => {
+          it('returns "score updated" to indicate that the score has been updated', (done) => {
+            request(server)
+              .patch('/api')
+              .send({ username: 'Test', score: 2 })
+              .expect('Content-Type', /application\/json/)
+              .expect(200)
+              .end((err, res) => {
+                if (err) return done(err);
+                if (res.body !== 'score updated')
+                  return done(
+                    `Expected response of "score updated". Instead received ${res.body}`
+                  );
+                return done();
+              });
+          });
+        });
+        describe('/api/updateProgress', () => {
+          it('returns updated user progress for a given user', (done) => {
+            request(server)
+              .patch('/api/updateProgress')
+              .send({ username: 'Test', progress: '2' })
+              .expect('Content-Type', /application\/json/)
+              .expect(200)
+              .end((err, res) => {
+                if (err) return done(err);
+                if (res.body.progress !== '2')
+                  return done(
+                    `Expected progress to be '2'. Instead received ${res.body.progress}`
+                  );
+                return done();
+              });
+          });
+        });
+      });
+    });
+  });
+  describe('GET /board', () => {
+    it('returns an array of objects', (done) => {
+      request(server)
+        .get('/board')
+        .expect('Content-Type', /application\/json/)
+        .expect(200)
+        .end((err, res) => {
+          if (err) return done(err);
+          if (!Array.isArray(res.body))
+            return done(
+              `Expected to receive and array. Instead received ${typeof res.body}`
+            );
+          if (res.body.length < 1)
+            return done(
+              `Expected array of length greater than 0. Instead received length of ${res.body.length}`
+            );
+          if (res.body[res.body.length - 1].username !== 'Test')
+            return done(
+              `Expected username of last element in array to be 'Test'. Instead received ${
+                res.body[res.body.length].username
+              }`
+            );
+          if (res.body[res.body.length - 1].score !== 2)
+            return done(
+              `Expected score of last element in array to be 2. Instead received ${
+                res.body[res.body.length].score
+              }`
+            );
+          return done();
+        });
+    });
+  });
+  describe('GET /user', () => {
+    it('returns score and progress for given user', (done) => {
+      request(server)
+        .get('/user?username=Test')
+        .expect('Content-Type', /application\/json/)
+        .expect(200)
+        .end((err, res) => {
+          if (err) return done(err);
+          if (res.body.score !== 2)
+            return done(
+              `Expected score to be 2. Instead received ${res.body.score}.`
+            );
+          if (res.body.progress !== '2')
+            return done(
+              `Expected progress to be 2. Instead received ${res.body.progress}`
+            );
+          return done();
+        });
+    });
+  });
+  describe('/* error handler unknown route', () => {
+    it('returns a status of 404 and sends back "THIS ENDPOINT DOES NOT EXIST', (done) => {
+      request(server)
+        .get('/testUnknown')
+        .expect('Content-Type', /text\/html/)
+        .expect(404)
+        .end((err, res) => {
+          if (err) return done(err);
+          if (res.text !== 'THIS ENDPOINT DOES NOT EXIST')
+            return done(
+              `Expected to receive "THIS ENDPOINT DOES NOT EXIST. Instead received ${res.body}`
+            );
+          return done();
+        });
+    });
+  });
   afterAll((done) => {
-    const query = "DELETE FROM user_final WHERE username = 'Test';"
-    db.query(query).then(()=> done());
-  })
+    const query = "DELETE FROM user_final WHERE username = 'Test';";
+    db.query(query).then(() => done());
+  });
 });
